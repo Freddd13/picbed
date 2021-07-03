45e6fc7322a34c134bc47ec4a9b29639cb079143
Freddd13 <yzdyzd13@gmail.com>
2021-07-03 14:18:29 +0800

    upload

--- scripts/generateTS.ps1
+++ scripts/generateTS.ps1
@@ -1,3 +1,12 @@
-$fn = "17sai.mp4"
-ffmpeg -i $fn -c:v libx264 -strict -2 ($fn.Substring(0, $fn.LastIndexOf('.'))+'out.mp4')
-ffmpeg -i ($fn.Substring(0, $fn.LastIndexOf('.'))+'.mp4') -c copy -map 0 -f segment -segment_list ($fn.Substring(0, $fn.LastIndexOf('.'))+'_playlist.m3u8') -segment_time 5 ($fn.Substring(0, $fn.LastIndexOf('.'))+'%03d.ts')
+param($fn = "", $fDir = "../video")
+if ($fn -eq ""){
+	echo "e.g. ./generateTS.ps1 17sai.mp4 ../video/"
+	exit
+}
+cd $fDir
+$fBasename = $fn.Substring(0, $fn.LastIndexOf('.'))
+ffmpeg -i $fn -c:v libx264 -strict -2 ($fBasename + 'out.mp4')
+ffmpeg -i ($fBasename +'out.mp4') -c copy -map 0 -f segment -segment_list ($fBasename + '_playlist.m3u8') -segment_time 5 ($fBasename + '%03d.ts')
+cd -
+echo ""
+echo ("jsdelivr url is:     https://cdn.jsdelivr.net/gh/Freddd13/picBed/" + $fBasename + "_playlist.m3u8")
